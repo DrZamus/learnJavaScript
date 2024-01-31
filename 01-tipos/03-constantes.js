2f33edbc06a30bd8c7d623099cbd323a5dd42bc7
Samuel <samuelmuan31@gmail.com>
2024-01-30 21:33:13 -0500

    Mi primer commit de HolaMundo

---
+++ 01-tipos/03-constantes.js
@@ -0,0 +1,4 @@
+const nombre = "Hola Mundo";
+nombre = "hola xd";
+
+console.log(nombre)
---
+++ 01-tipos/05-comentarios.js
@@ -0,0 +1,8 @@
+//hola esto es un comentariooooo
+
+
+/*other comment
+otro comentario*/
+
+
+let numberComment = 2; //este es un comentario de una sola línea
