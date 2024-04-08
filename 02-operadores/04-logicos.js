78c741713df1df137cc5b4cc3c0829f97969c73d
Samuel <samuelmuan31@gmail.com>
2024-04-07 22:02:46 -0500

    Mi tercer commit HolaMundo

---
+++ 02-operadores/04-logicos.js
@@ -0,0 +1,21 @@
+//AND, OR, NOT
+
+//AND &&
+
+// console.log(true && true);
+
+let mayor = true;
+let suscrito = true;
+
+
+console.log('operador and', mayor && suscrito);
+
+
+suscrito = false;
+mayor = false;
+//OR ||
+console.log('operador or', mayor || suscrito);
+
+//NOT !
+console.log('operador not', !mayor);
+let catalogoInfantil = !mayor;
---
+++ LogicExercises/test1.js
@@ -0,0 +1,6 @@
+function multi(a,b){
+    return a*b;
+}
+
+let X = '2 x 3 =' + multi(2,3);
+console.log(X);
